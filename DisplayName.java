df4b2d06a3c916f5b52f5fa121ebaa17d232b2eb
Aarti Rani <69513794+Aarti-Rani@users.noreply.github.com>
2024-05-17 07:02:25 +0000

    Update DisplayName.java

--- DisplayName.java
+++ DisplayName.java
@@ -1,5 +1,6 @@
 public class DisplayName{
 public static void main(String []args){
-System.out.println("Aarti");
+System.out.println("HI Aarti");
+  System.out.println("Welcome to the Jenkins Practices");
 }
 }
